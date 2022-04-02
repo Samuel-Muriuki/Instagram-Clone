697e476eb608abac7047aa68f666f44dfbf66af8
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-02 18:00:27 +0300

    Adding the register function to the views

--- ig/views.py
+++ ig/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
 
 # Create your views here.
 def homepage(request):
-    return render(request, "ig/home.html")
+    return render(request, "home.html")
+
+def register_request(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registartion successful")
+            return redirect("home")
+        messages.error(request, "Unsuccessful registration. Invalid information")
+    form = NewUserForm()
+    return render (request=request, template_name="ig/register.html", context={"register_form":form})
