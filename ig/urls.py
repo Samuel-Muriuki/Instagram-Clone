4bb73b5d9bb37df3b19055f216fa3110990a9add
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-02 18:07:48 +0300

    Adding the login url to the app

--- ig/urls.py
+++ ig/urls.py
@@ -4,5 +4,6 @@ from . import views
 # URL Configurations
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("register", views.register_request, name="register")
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
 ]
