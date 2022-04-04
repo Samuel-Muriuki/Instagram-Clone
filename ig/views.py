f8b39975701e85817af1ffbccc7e4632088e53d6
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-04 08:44:29 +0300

    Creating the logic to edit a profile

--- ig/views.py
+++ ig/views.py
@@ -111,3 +111,28 @@ def post(request):
     else:
         form = NewPostForm()
     return render(request, 'ig/post.html', {"form": form})
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
+
+        if user_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+            return redirect('homepage')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user)
+
+        context = {
+            'user_form': user_form,
+            'profile_form': profile_form
+
+        }
+
+    return render(request, 'ig/edit_profile.html', context)
