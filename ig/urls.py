20392bda60db47c5eab6661b75c4fab1c04c97bc
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-04 08:45:05 +0300

    Creating the edit profile url

--- ig/urls.py
+++ ig/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("logout/", views.logout_request, name= "logout"),
     path('profile/', views.profile,name = 'profile'),
     path("post", views.post, name="post"),
-    
+    path('edit_profile/', views.edit_profile,name = 'edit_profile'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
