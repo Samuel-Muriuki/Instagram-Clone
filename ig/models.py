4d54225fe457650712c8cb9762bf5c185aaf41dd
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-04 08:28:55 +0300

    Updating the Profile, login and register templates

--- ig/models.py
+++ ig/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_info = models.TextField()
+    picture = models.ImageField(upload_to='post/', blank=True,default='')
+    name = models.CharField(max_length=50)
+
+    
