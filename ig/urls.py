f817f6845a66af8f709ef1c8258d4796815567ee
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-04 09:53:21 +0300

    Creating the profile url link

--- ig/urls.py
+++ ig/urls.py
@@ -7,12 +7,12 @@ from django.conf import settings
 urlpatterns = [
     path("", views.login_request, name="login"),
     path("homepage", views.homepage, name="homepage"),
-    path("register/", views.register_request, name="register"),
-    path("login/", views.login_request, name="login"),
-    path("logout/", views.logout_request, name= "logout"),
-    path('profile/', views.profile,name = 'profile'),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name= "logout"),
+    path('profile', views.profile,name = 'profile'),
     path("post", views.post, name="post"),
-    path('edit_profile/', views.edit_profile,name = 'edit_profile'),
+    path('edit_profile', views.edit_profile,name = 'edit_profile'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
