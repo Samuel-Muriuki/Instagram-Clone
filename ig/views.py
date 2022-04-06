2ba6e823ffb2c39513d0ddea9882d0f6e3fede14
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-06 08:41:25 +0300

    UNRESPONSIVE IMAGES

--- ig/views.py
+++ ig/views.py
@@ -12,6 +12,7 @@ from django.http import HttpResponseRedirect
 
 # Create your views here.
 
+
 @login_required(login_url='login/')
 def homepage(request):
     posts = Post.all_posts()
@@ -21,11 +22,11 @@ def homepage(request):
 
     comments = Comment.objects.all()
     likes = Like.objects.all()
-    for post in posts:                                                            
+    for post in posts:
         num_likes = 0
         for like in likes:
             if post.id == like.post.id:
-                num_likes +=1
+                num_likes += 1
         post.likes = num_likes
         post.save()
         pic = Profile.objects.filter(username=post.user.id).first()
@@ -42,12 +43,12 @@ def homepage(request):
 
         )
         json_posts.append(obj)
-        
+
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             post_id = int(request.POST.get("idpost"))
-            post = Post.objects.get(id = post_id)
+            post = Post.objects.get(id=post_id)
             comment = form.save(commit=False)
             comment.username = request.user
             comment.post = post
@@ -57,10 +58,10 @@ def homepage(request):
     else:
         form = CommentForm()
 
-    
     likes = Like.objects.all()
-    
-    return render(request, 'home.html', {"posts": json_posts, "likes":likes, "comments":comments, "profiles":profiles,"current_user":current_user,})
+
+    return render(request, 'home.html', {"posts": json_posts, "likes": likes, "comments": comments, "profiles": profiles, "current_user": current_user, })
+
 
 def register_request(request):
     if request.method == 'POST':
@@ -70,32 +71,36 @@ def register_request(request):
             login(request, user)
             messages.success(request, "Registartion successful")
             return redirect("login")
-        messages.error(request, "Unsuccessful registration. Invalid information")
+        messages.error(
+            request, "Unsuccessful registration. Invalid information")
     form = NewUserForm()
-    return render (request=request, template_name="ig/register.html", context={"register_form":form})
+    return render(request=request, template_name="ig/register.html", context={"register_form": form})
+
 
 def login_request(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("homepage")
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
-	return render(request=request, template_name="ig/login.html", context={"login_form": form})
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("homepage")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="ig/login.html", context={"login_form": form})
+
 
 def logout_request(request):
-	logout(request)
-	messages.info(request, "You have successfully logged out.") 
-	return redirect("login")
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("login")
+
 
 @login_required(login_url='login')
 def profile(request):
@@ -105,19 +110,19 @@ def profile(request):
         profile_form = ProfileUpdateForm(
             request.POST, request.FILES, instance=request.user)
 
-        if  profile_form.is_valid():
+        if profile_form.is_valid():
             user_form.save()
             profile_form.save()
 
             return redirect('homepage')
 
     else:
-        
+
         profile_form = ProfileUpdateForm(instance=request.user)
         user_form = UserUpdateForm(instance=request.user)
 
         context = {
-            'user_form':user_form,
+            'user_form': user_form,
             'profile_form': profile_form
 
         }
@@ -140,31 +145,34 @@ def user_profile(request, username):
             if_follow = False
 
     print(followers)
-    return render(request, 'ig/posts.html', {'user_poster': user_poster,'followers': followers, 'if_follow': if_follow,'user_posts':user_posts})
+    return render(request, 'ig/posts.html', {'user_poster': user_poster, 'followers': followers, 'if_follow': if_follow, 'user_posts': user_posts})
+
 
 def post(request):
     current_user = request.user
-   
+
     if request.method == 'POST':
         form = NewPostForm(request.POST, request.FILES)
         if form.is_valid():
             image = form.save(commit=False)
             image.user = current_user
-           
+
             image.save()
-            
+
         return redirect('homepage')
 
     else:
         form = NewPostForm()
     return render(request, 'ig/post.html', {"form": form})
 
+
 @login_required(login_url='login')
 def edit_profile(request):
     if request.method == 'POST':
 
         user_form = UserUpdateForm(request.POST, instance=request.user)
-        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user)
 
         if user_form.is_valid():
             user_form.save()
@@ -184,6 +192,7 @@ def edit_profile(request):
 
     return render(request, 'ig/edit_profile.html', context)
 
+
 @login_required(login_url='login')
 def comment(request, id):
     image = Post.objects.get(id=id)
@@ -195,13 +204,14 @@ def comment(request, id):
             new_comment.image = image
             new_comment.user = request.user.profile
             new_comment.save()
-            
+
             return HttpResponseRedirect(request.path_info)
-            
+
     else:
         form = CommentForm()
 
-    return render(request, 'ig/post.html', {'post': image,'form': form, 'comment_content': comment_content})
+    return render(request, 'ig/post.html', {'post': image, 'form': form, 'comment_content': comment_content})
+
 
 @login_required(login_url='login')
 def search(request):
@@ -212,6 +222,6 @@ def search(request):
         results = User.objects.filter(username__icontains=search_term)
         print(results)
 
-        return render(request, 'ig/search.html',locals())
+        return render(request, 'ig/search.html', locals())
 
     return redirect('homepage')
